f8e6f7760df073d34228618fa2bcd3f8f5153e12
NexusCh <1482763380@qq.com>
2021-07-14 15:50:53 +0800

    首页部分完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,24 @@
 <template>
     <div id="home">
         <nav-bar class="home-nav"><template v-slot:center><div>购物街</div></template></nav-bar>
+        <tab-control class="tabControl" 
+            :titles="['流行','新款','精选']" 
+            @tabClick="tabClick"
+            ref="tabControl1"
+            v-show="isTabFixed"/>
         <scroll class="content" 
             ref="scroll" 
             :probe-type="3" 
             @scroll="contentScroll"
             :pull-up-load="true"
             @pullingUp="loadMore">
-            <home-swiper :banners="banners"/>
+            <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
             <home-recommend-view :recommends="recommends"/>
             <feature-view />
             <tab-control class="tab-control" 
                         :titles="['流行','新款','精选']" 
-                        @tabClick="tabClick"/>
+                        @tabClick="tabClick"
+                        ref="tabControl2"/>
             <goods-list :goods="showGoods"/>
         </scroll>
         <back-top @click="backClick" v-show="isShowBackTop"/>
@@ -31,6 +37,7 @@
     import BackTop from 'components/content/backTop/BackTop'
 
     import {getHomeMultidata, getHomeGoods} from 'network/home.js'
+    import {bebounce} from 'common/utils'
 
     export default {
         name:'Home',
@@ -52,6 +59,7 @@
             this.getHomeGoods('pop')
             this.getHomeGoods('new')
             this.getHomeGoods('sell')
+
         },
         methods: {
             /* 事件监听相关方法 */
@@ -67,16 +75,25 @@
                         this.currentType = 'sell';
                         break;
                 }
+                this.$refs.tabControl1.currentIndex = index;
+                this.$refs.tabControl2.currentIndex = index;
             },
             backClick(){
                 this.$refs.scroll.scrollTo(0, 0);
             },
             contentScroll(position){
-                this.isShowBackTop = position.y < -1000 ? true : false;
+                // 1. 判断 BackTop 是否显示
+                this.isShowBackTop = position.y < -1000;
+
+                // 2. 决定 tabControl 是否吸顶 (position: fixed)
+                this.isTabFixed = position.y < -this.tabOffsetTop
             },
             loadMore(){
+                // 加载更多
                 this.getHomeGoods(this.currentType);
-
+            },
+            swiperImageLoad(){
+                this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop - 44;
             },
 
             /* 网络请求相关方法 */
@@ -118,13 +135,30 @@
                     }
                 },
                 currentType: 'pop',
-                isShowBackTop: false
+                isShowBackTop: false,
+                tabOffsetTop: 0,
+                isTabFixed: false,
+                saveY: 0
             }
         },
         computed: {
             showGoods(){
                 return this.goods[this.currentType].list
             }
+        },
+        mounted(){
+            // 获取 tabControl的offsetTop
+            // 所有的组件都有一个属性 $el ，用于获取组件中的元素  
+        },
+        destroyed(){
+            console.log('your home has been destroyed!')
+        },
+        activated(){
+            this.$refs.scroll.scrollTo(0, this.saveY, 1000);
+            // this.$refs.scroll.scroll.refresh();
+        },
+        deactivated(){
+            this.saveY = this.$refs.scroll.getScrollY();
         }
     }
 </script>
@@ -150,14 +184,16 @@
         width: 100%;
     }
 
-    .tab-control {
-        position: sticky;
-        top: 44px;
-        z-index: 1;
-    }
-
     .content {
         height: 100%;
         overflow: hidden;
+    }
+
+    .tabControl {
+        position: absolute;
+        top: 44px;
+        left: 0;
+        right: 0;
+        z-index: 9;
     }
 </style>
