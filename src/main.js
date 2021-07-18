c153d8a37189c55276f2e833a8428ce58026148e
NexusCh <1482763380@qq.com>
2021-07-18 22:54:31 +0800

    修复少许bug

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'
+
 import toast from 'components/common/toast'
 
-createApp(App).use(store).use(router).use(toast).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(toast).mount('#app')
+    // 解决移动端 300 ms 延迟
+FastClick.attach(document.body)
+
+// 未能解决 bug ： vue-lazyload 的使用
